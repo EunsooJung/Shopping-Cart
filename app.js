bc2992eae02faab9b79b7ee58b48b0d6e82b6953
EunsooJung <hi.michael.jung@gmail.com>
2020-02-09 03:10:10 -0800

    Updated port no.

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 ﻿'use strict';
-var debug = require('debug');
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -12,12 +11,12 @@ var routerRegister = require('./routers/router.register.js');
 var session = require('express-session');
 var contextProcessor = require('./middlewares/context.processor.js');
 
-
 var app = express();
 
 // View Engine SetUp
 nunjucks.configure(
-  ['views',
+  [
+    'views',
     'views/cataloguedm',
     'views/cataloguedm/partials',
     'views/cartdm',
@@ -27,7 +26,8 @@ nunjucks.configure(
   {
     autoescape: true,
     express: app
-  });
+  }
+);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -44,25 +44,26 @@ app.use(express.static(path.join(__dirname, 'public')));
 //Pagination Middleware
 app.use(paginate.middleware(9, 20));
 
-app.use(session({
-  secret: '95371e2f-a487-4e22-a9e2-8b6356b85453',
-  proxy: true,
-  resave: true,
-  saveUninitialized: true
-}));
+app.use(
+  session({
+    secret: '95371e2f-a487-4e22-a9e2-8b6356b85453',
+    proxy: true,
+    resave: true,
+    saveUninitialized: true
+  })
+);
 
 //Make the cart parameters available to the templates
 app.use(contextProcessor.localContext);
-
 
 // Register the Routers
 routerRegister(app);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // error handlers
@@ -70,27 +71,27 @@ app.use(function (req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function (err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
     });
+  });
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function (err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 2001);
 
-var server = app.listen(app.get('port'), function () {
-    debug('Express server listening on port ' + server.address().port);
+var server = app.listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + server.address().port);
 });
